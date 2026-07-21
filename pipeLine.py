b26ecbbffa68276a0a8573a19cc4f50459b9e195
AnujrajShrestha <shrestharachana196@gmail.com>
2026-07-21 22:44:09 +0545

    new

--- pipeLine.py
+++ pipeLine.py
@@ -1,44 +1,9 @@
 from agents import build_eda_agent,build_visualization_agent,build_correaltion_agent,build_summary_agent
+from create_report import create_report
 from langchain_mistralai import MistralAIEmbeddings
 from pathlib import Path
 
 embedding_model = MistralAIEmbeddings(model="mistral-embed")
-
-def create_report(state: dict):
-
-    separator = "\n" + "-" * 60 + "\n"
-
-    with open("report.txt", "w", encoding="utf-8") as fs:
-
-        fs.write(separator)
-        fs.write("Step 1 - EDA Agent\n")
-        fs.write(separator)
-
-        for key, value in state["eda_result"].model_dump().items():
-            fs.write(f"{key}: {value}\n")
-
-        fs.write(separator)
-        fs.write("Step 2 - Visualization Agent\n")
-        fs.write(separator)
-
-        for key, value in state["visual_result"].model_dump().items():
-            fs.write(f"{key}: {value}\n")
-
-        fs.write(separator)
-        fs.write("Step 3 - Correlation Agent\n")
-        fs.write(separator)
-
-        for key, value in state["corr_result"].model_dump().items():
-            fs.write(f"{key}: {value}\n")
-
-        fs.write(separator)
-        fs.write("Step 4 - Summary Agent\n")
-        fs.write(separator)
-
-        for key, value in state["summary_result"].model_dump().items():
-            fs.write(f"{key}: {value}\n")
-
-    return "report.txt"
 
 def run_pipeline(query: str) -> dict:
     state={}
